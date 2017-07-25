573526f621164be3e10bc5ee06b68ef70163c4c2
4lch3-m1st <4lch3m1st@protonmail.com>
2017-07-25 09:27:16 -0300

    Started experimenting with ncurses.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <ncurses.h>
+#include <unistd.h>
+#include <string.h>
+
+static int MAX_X, MAX_Y;
+
+#define centertext(text) \
+    mvprintw(MAX_Y/2, (MAX_X/2) - (strlen(text)/2), text)
 
 int main(int argc, char** argv)
 {
-    puts("Hello, world!");
+    initscr();
+    noecho();
+    curs_set(FALSE);
+
+    getmaxyx(stdscr, MAX_Y, MAX_X);
+    centertext("Hello, world!");
+    refresh();
+
+    sleep(1);
+    
+    endwin();
     return 0;
 }
